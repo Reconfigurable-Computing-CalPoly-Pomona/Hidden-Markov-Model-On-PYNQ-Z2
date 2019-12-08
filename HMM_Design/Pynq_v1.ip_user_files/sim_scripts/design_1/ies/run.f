d13f098ce9e2a1ed4052bb3daa38b408e1479561
Aaron Zhang <43483348+AaronXFZ@users.noreply.github.com>
2019-12-08 14:37:40 -0800

    Add files via upload

---
+++ HMM_Design/Pynq_v1.ip_user_files/sim_scripts/design_1/ies/run.f
@@ -0,0 +1,137 @@
+-makelib ies_lib/xilinx_vip -sv \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/axi4stream_vip_axi4streampc.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/axi_vip_axi4pc.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/xil_common_vip_pkg.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/axi4stream_vip_pkg.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/axi_vip_pkg.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/axi4stream_vip_if.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/axi_vip_if.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/clk_vip_if.sv" \
+  "E:/vivado/Vivado/2019.1/data/xilinx_vip/hdl/rst_vip_if.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "E:/vivado/Vivado/2019.1/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "E:/vivado/Vivado/2019.1/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv" \
+  "E:/vivado/Vivado/2019.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "E:/vivado/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/axi_infrastructure_v1_1_0 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_vip_v1_1_5 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/d4a8/hdl/axi_vip_v1_1_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/processing_system7_vip_v1_0_7 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/8c62/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_processing_system7_0_2/sim/design_1_processing_system7_0_2.v" \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/073c/hdl/verilog/HMM_Scoring_AXILiteS_s_axi.v" \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/073c/hdl/verilog/HMM_Scoring_gmem_m_axi.v" \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/073c/hdl/verilog/HMM_Scoring_gradicud.v" \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/073c/hdl/verilog/HMM_Scoring_max_abkb.v" \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/073c/hdl/verilog/HMM_Scoring.v" \
+  "../../../bd/design_1/ip/design_1_HMM_Scoring_0_3/sim/design_1_HMM_Scoring_0_3.v" \
+-endlib
+-makelib ies_lib/lib_cdc_v1_0_2 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/ef1e/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib ies_lib/proc_sys_reset_v5_0_13 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/8842/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_rst_ps7_0_100M_2/sim/design_1_rst_ps7_0_100M_2.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/sim/bd_6e42.v" \
+-endlib
+-makelib ies_lib/xlconstant_v1_1_6 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/66e7/hdl/xlconstant_v1_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_0/sim/bd_6e42_one_0.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_1/sim/bd_6e42_psr_aclk_0.vhd" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/1ddd/hdl/sc_util_v1_0_vl_rfs.sv" \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/f85e/hdl/sc_mmu_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_2/sim/bd_6e42_s00mmu_0.sv" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/ca72/hdl/sc_transaction_regulator_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_3/sim/bd_6e42_s00tr_0.sv" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/7de4/hdl/sc_si_converter_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_4/sim/bd_6e42_s00sic_0.sv" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/b89e/hdl/sc_axi2sc_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_5/sim/bd_6e42_s00a2s_0.sv" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/b2d0/hdl/sc_node_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_6/sim/bd_6e42_sarn_0.sv" \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_7/sim/bd_6e42_srn_0.sv" \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_8/sim/bd_6e42_sawn_0.sv" \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_9/sim/bd_6e42_swn_0.sv" \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_10/sim/bd_6e42_sbn_0.sv" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/7005/hdl/sc_sc2axi_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_11/sim/bd_6e42_m00s2a_0.sv" \
+-endlib
+-makelib ies_lib/smartconnect_v1_0 -sv \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/b387/hdl/sc_exit_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib -sv \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/bd_0/ip/ip_12/sim/bd_6e42_m00e_0.sv" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_axi_smc_2/sim/design_1_axi_smc_2.v" \
+-endlib
+-makelib ies_lib/generic_baseblocks_v2_1_0 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/b752/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_4 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/1f5a/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_4 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/1f5a/hdl/fifo_generator_v13_2_rfs.vhd" \
+-endlib
+-makelib ies_lib/fifo_generator_v13_2_4 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/1f5a/hdl/fifo_generator_v13_2_rfs.v" \
+-endlib
+-makelib ies_lib/axi_data_fifo_v2_1_18 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/5b9c/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_register_slice_v2_1_19 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/4d88/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/axi_protocol_converter_v2_1_19 \
+  "../../../../Pynq_v1.srcs/sources_1/bd/design_1/ipshared/c83a/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../bd/design_1/ip/design_1_auto_pc_0_1/sim/design_1_auto_pc_0.v" \
+  "../../../bd/design_1/sim/design_1.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
